e55261998308f0afef83fc3048fb3e710400abf5
TiagoLuizSS <109998372+TiagoLuizSS@users.noreply.github.com>
2022-09-18 22:23:42 -0300

    ToDo_5, completo ...

---
+++ DML-Updates.sql
@@ -0,0 +1,37 @@
+use ToDo5;
+
+UPDATE produto 
+SET 
+    cor = 'Vermelho',
+    modelo = 'Iglu'
+WHERE
+    id = '000148881';
+
+UPDATE produto 
+SET 
+    cor = 'Amarelo',
+    modelo = 'Canadense'
+WHERE
+    id = '000148882';
+
+UPDATE produto 
+SET 
+    cor = 'Verde',
+    modelo = 'Geodésica'
+WHERE
+    id = '000148883';
+
+UPDATE produto 
+SET 
+    cor = 'Preto',
+    modelo = 'Armação por Polos'
+WHERE
+    id = '000148884';
+
+UPDATE produto 
+SET 
+    cor = 'Laranja',
+    modelo = 'Articus'
+WHERE
+    id = '000148885';
+
